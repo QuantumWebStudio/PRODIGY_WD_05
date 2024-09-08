faf1a65ec61d97f2946a95ce51a71beb041525b5
Ziyaan Ahmed <ziyaanahmed@icloud.com>
2024-09-08 22:47:59 +0530

    first Commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # PRODIGY_WD_05
-In this Repo it contains a Weather Application where the users can enter the city name and in response it will send back the weather details of that particular city. 
+
+This Repo contains a Weather Application where the users can enter the city name and in response it will send back the weather details of that particular city.
+<br>
+<br>
+In this project i have used
+
+<ul>
+    <li>
+        TailwindCSS as a CSS Framework.
+    </li>
+    <li>
+        FontAwesome for Icons.
+    </li>
+    <li>
+        Weather API for getting the weather Information
+    </li>
+    <li>
+        Google Fonts for Fonts
+    </li>
+</ul>
---
+++ src/main.js
@@ -0,0 +1,83 @@
+const errorSection = document.getElementById("errorSection");
+const displayInfo = document.getElementById("displayInfo");
+const sendDetails = () => {
+  console.log("WORKING!!");
+  const cityName = document.getElementById("city").value;
+  console.log(cityName, "FROM CITYNAME");
+  getWeatherDetails(cityName);
+};
+
+const getWeatherDetails = async (cityName) => {
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=d403f92fca4e4377995134234241704&q=${cityName}&aqi=yes`
+    );
+    const weatherData = await response.json();
+    if (response.status === 400) {
+      errorSection.classList.remove("hidden");
+      displayInfo.classList.remove("flex");
+      displayInfo.classList.add("hidden");
+      errorSection.classList.add("flex");
+      throw new Error("City not found");
+    }
+    if (response.status === 200) {
+      displayWeatherDetails(weatherData);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const displayWeatherDetails = (weatherData) => {
+  errorSection.classList.remove("flex");
+  errorSection.classList.add("hidden");
+  displayInfo.classList.remove("hidden");
+  displayInfo.classList.add("flex");
+  console.log(weatherData);
+  const { temp_c, temp_f, humidity, is_day, wind_kph } = weatherData.current;
+  const { country, localtime, name, region } = weatherData.location;
+  const displayCity = (document.getElementById("displayCity").innerText = name);
+  const displayCountry = (document.getElementById("displayCountry").innerText =
+    country);
+  const displayRegion = (document.getElementById("displayRegion").innerText =
+    region);
+  const displayDateTime = (document.getElementById("displayDateTime").innerText =
+    localtime);
+  const displayTempC = (document.getElementById(
+    "displayTempC"
+  ).innerText = `${temp_c}°C`);
+  const displayWindSpeend = (document.getElementById(
+    "displayWindSpeend"
+  ).innerText = `${wind_kph}kmph`);
+  const displayTempF = (document.getElementById(
+    "displayTempF"
+  ).innerText = `${temp_f}°F`);
+  const displayHumidity = (document.getElementById("displayHumidity").innerText =
+    humidity);
+  const displayDay = document.getElementById("displayDay");
+  switch (is_day) {
+    case 0:
+      displayDay.innerText = "Sunday";
+      break;
+    case 1:
+      displayDay.innerText = "Monday";
+      break;
+    case 2:
+      displayDay.innerText = "Tuesday";
+      break;
+    case 3:
+      displayDay.innerText = "Wednesday";
+      break;
+    case 4:
+      displayDay.innerText = "Thursday";
+      break;
+    case 5:
+      displayDay.innerText = "Friday";
+      break;
+    case 6:
+      displayDay.innerText = "Saturday";
+      break;
+    default:
+      break;
+  }
+};
